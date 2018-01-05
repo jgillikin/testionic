b4c027403679ae492e4cbd8233e8496b39acb2bf
Jason Gillikin <jgillikin@USIT.LOCAL>
2018-01-05 10:01:29 -0500

    po4

--- src/pages/homeadd/homeadd.ts
+++ src/pages/homeadd/homeadd.ts
@@ -204,10 +204,10 @@ var dd = today.getDate();
 var mm = today.getMonth()+1; //January is 0!
 
 var yyyy = today.getFullYear();
-if(dd<10){
+if(eval(dd)<10){
     dd='0'+dd;
 } 
-if(mm<10){
+if(eval(mm)<10){
     mm='0'+mm;
 } 
 var today = mm+'/'+dd+'/'+yyyy;
